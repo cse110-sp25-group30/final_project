baedc098219d4b3cce2c60a084a98f842f1aaff9
KevinISun <114264552+KevinISun@users.noreply.github.com>
2025-05-09 21:05:29 +0000

    adding some things to the card function

--- index.js
+++ index.js
@@ -4,5 +4,7 @@
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
  */
-function Book(title, author) {
+function Card(title, author) {
+  this.title = title;
+  this.author = author;
 }
